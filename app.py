73602b8d3677e6618bfacad2f73873fbc8d2bfaf
Zullu <karamerSeinfeld@gmail.com>
2019-12-13 16:11:36 +0500

    finished except doesn't show the full picture if define instart function

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ def changeState(initial, final):
 
 
 def move(row, column):
+    print("State Before\n", state)
     if row == 1 and column == 0:
         if (state[1] == 8):
             changeState(1, 0)
@@ -77,7 +78,7 @@ def move(row, column):
             swap({"row": row, "column": column}, {"row": 2, "column": 2})
         elif (state[1] == 8):
             changeState(1, 4)
-            swap({"row": row, "column": column}, {"row": 1, "column": 2})
+            swap({"row": row, "column": column}, {"row": 1, "column": 1})
         elif (state[3] == 8):
             changeState(3, 4)
             swap({"row": row, "column": column}, {"row": 2, "column": 0})
@@ -119,6 +120,26 @@ def move(row, column):
             changeState(7, 8)
             swap({"row": row, "column": column}, {"row": 3, "column": 1})
 
+    print("State Before\n", state)
+
+
+def checker():
+    global state
+    for x in state:
+        if(x == state[x]):
+            if x == 8:
+                print("\n\n\n\n\t\t\tYou've won the game.")
+                winBox = Tk()
+                global imgs, imgs_con
+                state = []
+                imgs = []
+                imgs_con = []
+                start()
+                Button(winBox, text="You've win the game!", font=("Helvitica", 28), fg="#11ff11", command=lambda: winBox.destroy()).grid(
+                    row=0, column=0, padx=10, pady=10, ipadx=100, ipady=50)
+        else:
+            break
+
 
 def swap(intial, final):
     ir = intial['row']
@@ -129,66 +150,81 @@ def swap(intial, final):
     indexF = 3*(fr - 1) + fc
     global imgs, imgs_con
 
-    imgs[indexI] = ImageTk.PhotoImage(Image.open(
-        './images/Layer ' + str(indexI) + '.jpg'))
+    print("initial\t", intial)
+    print("final\t", final)
 
-    imgs[indexF] = ImageTk.PhotoImage(Image.open(
+    print("IndexI\t", indexI)
+    print("IndexF\t", indexF)
+
+    imgs[indexI] = ImageTk.PhotoImage(Image.open(
         './images/Layer 8.jpg'))
 
-    imgs_con[indexI] = Button(root, image=imgs[indexI])
-    imgs_con[indexF] = Button(root, image=imgs[indexF])
+    imgs[indexF] = ImageTk.PhotoImage(Image.open(
+        './images/Layer '+str(state[indexF]) + '.jpg'))
 
-    imgs_con[indexI].grid(row=fr, column=fc)
-    imgs_con[indexF].grid(row=ir, column=ic)
+    imgs_con[indexI] = Button(root, image=imgs[indexI],
+                              command=lambda: move(ir, ic))
+    imgs_con[indexF] = Button(root, image=imgs[indexF],
+                              command=lambda: move(fr, fc))
 
-
-for x in range(9):
-    randomlyFill()
-
-for x in range(9):
-    imgs.append(ImageTk.PhotoImage(Image.open(
-        './images/Layer ' + str(state[x]) + '.jpg')))
-
-img_con_0 = Button(root, image=imgs[0], command=lambda: move(1, 0))
-imgs_con.append(img_con_0)
-imgs_con[0].grid(row=1, column=0)
-
-img_con_1 = Button(root, image=imgs[1], command=lambda: move(1, 1))
-imgs_con.append(img_con_1)
-imgs_con[1].grid(row=1, column=1)
-
-img_con_2 = Button(root, image=imgs[2], command=lambda: move(1, 2))
-imgs_con.append(img_con_2)
-imgs_con[2].grid(row=1, column=2)
-
-img_con_3 = Button(root, image=imgs[3], command=lambda: move(2, 0))
-imgs_con.append(img_con_3)
-imgs_con[3].grid(row=2, column=0)
-
-img_con_4 = Button(root, image=imgs[4], command=lambda: move(2, 1))
-imgs_con.append(img_con_4)
-imgs_con[4].grid(row=2, column=1)
-
-img_con_5 = Button(root, image=imgs[5], command=lambda: move(2, 2))
-imgs_con.append(img_con_5)
-imgs_con[5].grid(row=2, column=2)
-
-img_con_6 = Button(root, image=imgs[6], command=lambda: move(3, 0))
-imgs_con.append(img_con_6)
-imgs_con[6].grid(row=3, column=0)
-
-img_con_7 = Button(root, image=imgs[7], command=lambda: move(3, 1))
-imgs_con.append(img_con_7)
-imgs_con[7].grid(row=3, column=1)
+    imgs_con[indexI].grid(row=ir, column=ic)
+    imgs_con[indexF].grid(row=fr, column=fc)
+    checker()
 
 
-img_con_8 = Button(root, image=imgs[8], command=lambda: move(3, 2))
-imgs_con.append(img_con_8)
-imgs_con[8].grid(row=3, column=2)
+def start():
+    global state, imgs, imgs_con, root
 
-full_img = ImageTk.PhotoImage(Image.open('./images/full_pic.jpg'))
-full_img_con = Label(root, image=full_img)
-full_img_con.grid(row=1, column=3, columnspan=2, rowspan=3)
+    for x in range(9):
+        randomlyFill()
 
+    for x in range(9):
+        imgs.append(ImageTk.PhotoImage(Image.open(
+            './images/Layer ' + str(state[x]) + '.jpg')))
+
+    # full image
+    full_img = ImageTk.PhotoImage(Image.open('./images/full_pic.jpg'))
+    full_img_con = Label(root, image=full_img)
+    full_img_con.grid(row=1, column=3, columnspan=2, rowspan=3)
+
+    # other imgs
+    img_con_0 = Button(root, image=imgs[0], command=lambda: move(1, 0))
+    imgs_con.append(img_con_0)
+    imgs_con[0].grid(row=1, column=0)
+
+    img_con_1 = Button(root, image=imgs[1], command=lambda: move(1, 1))
+    imgs_con.append(img_con_1)
+    imgs_con[1].grid(row=1, column=1)
+
+    img_con_2 = Button(root, image=imgs[2], command=lambda: move(1, 2))
+    imgs_con.append(img_con_2)
+    imgs_con[2].grid(row=1, column=2)
+
+    img_con_3 = Button(root, image=imgs[3], command=lambda: move(2, 0))
+    imgs_con.append(img_con_3)
+    imgs_con[3].grid(row=2, column=0)
+
+    img_con_4 = Button(root, image=imgs[4], command=lambda: move(2, 1))
+    imgs_con.append(img_con_4)
+    imgs_con[4].grid(row=2, column=1)
+
+    img_con_5 = Button(root, image=imgs[5], command=lambda: move(2, 2))
+    imgs_con.append(img_con_5)
+    imgs_con[5].grid(row=2, column=2)
+
+    img_con_6 = Button(root, image=imgs[6], command=lambda: move(3, 0))
+    imgs_con.append(img_con_6)
+    imgs_con[6].grid(row=3, column=0)
+
+    img_con_7 = Button(root, image=imgs[7], command=lambda: move(3, 1))
+    imgs_con.append(img_con_7)
+    imgs_con[7].grid(row=3, column=1)
+
+    img_con_8 = Button(root, image=imgs[8], command=lambda: move(3, 2))
+    imgs_con.append(img_con_8)
+    imgs_con[8].grid(row=3, column=2)
+
+
+start()
 
 root.mainloop()
